7d3b6f4f9b27f3cf27dabcba92af8613a3d9c0d8
Rishi-k-s <rishikrishna.sr@gmail.com>
2024-09-01 13:08:21 +0530

    added the inital files

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import ButtonGradient from "./assets/svg/ButtonGradient";
 
-export default function App() {
+const App = () => {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
-}
+    <>
+      <h1 className="text-3xl font-bold underline">Uvin</h1>;
+      <ButtonGradient />
+    </>
+  );
+};
